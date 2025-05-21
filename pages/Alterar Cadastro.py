518f4ee12a27a95d6e185d7a7ce72b10dbf632c7
Fabiano Luizon Campos <fabianoluizoncampos@gmail.com>
2025-05-21 16:51:16 -0300

    Alterar Dados ok

--- pages/Alterar Cadastro.py
+++ pages/Alterar Cadastro.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import sql_funcoes as sf
 
+dados = [ ["","","",""]]        
+
+
+# print(dados[0][1])
+
 st.set_page_config(page_title="Sistema de Cadastro de Cliente", page_icon=":bar_chart:", layout="wide")
 
 # Título da aplicação
@@ -25,29 +30,30 @@ if st.button("Buscar"):
 
 with st.form ("Alterar Cadastro de Cliente"):
     
-    text_input_nome = st.text_input("Nome", value=dados[0][1])
-    text_input_sobrenome = st.text_input("Sobrenome", value=dados[0][2])          
-    text_input_email = st.text_input("Email", value=dados[0][3])
+    if dados not in st.session_state:
+        text_input_nome = st.text_input("Nome", value=dados[0][1])
+        text_input_sobrenome = st.text_input("Sobrenome", value=dados[0][2])          
+        text_input_email = st.text_input("Email", value=dados[0][3])
 
-    if st.form_submit_button("Atualizar"):
-        if text_input_nome and text_input_sobrenome and text_input_email:
-            nomeDB = "cadastro.db"
-            nomeTabela = "Clientes"
+        if st.form_submit_button("Atualizar"):
+            if text_input_nome and text_input_sobrenome and text_input_email:
+                nomeDB = "cadastro.db"
+                nomeTabela = "Clientes"
 
-            # Inserir dados na tabela
-            registro = {        
-                    "nome": text_input_nome,
-                    "sobrenome": text_input_sobrenome,
-                    "email": text_input_email,        
-            }
+                # Inserir dados na tabela
+                registro = {        
+                        "nome": text_input_nome,
+                        "sobrenome": text_input_sobrenome,
+                        "email": text_input_email,        
+                }
 
-            print(text_input_name)
+                print(text_input_name)
 
-            st.write("Registro:", registro)
-            
-            # sf.atualizarDados(nomeDB, nomeTabela, registro)        
-            st.success("Cliente cadastrado com sucesso!")
+                st.write("Registro:", registro)
+                
+                # sf.atualizarDados(nomeDB, nomeTabela, registro)        
+                st.success("Cliente cadastrado com sucesso!")
 
-            
-        else:
-            st.error("Por favor, preencha todos os campos.")
+                
+            else:
+                st.error("Por favor, preencha todos os campos.")
